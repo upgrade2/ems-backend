6cacfeec0d06949d3621e4c6761ef5a83969a2f9
Ajay <ajay.singh.114085@gmail.com>
2024-11-07 00:07:41 +0530

    Added Repo

---
+++ src/main/java/com/example/emsbackend/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.emsbackend.repository;
+
+import com.example.emsbackend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
